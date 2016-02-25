0e7b9ceef034d70b12f22e281dbb815898499562
DMieshkov <d.mshkv@gmail.com>
2016-02-25 17:42:57 +0200

    traq from ftp

---
+++ traq/vendor/traq/config/database.php
@@ -0,0 +1,10 @@
+<?php
+$db = array(
+    'driver' => "pdo",
+    'type' => "mysql",
+    'host' => "127.0.0.1",
+    'username' => "pjxbteey_trauser",
+    'password' => "user132",
+    'database' => "pjxbteey_traq",
+    'prefix' => "",
+);
